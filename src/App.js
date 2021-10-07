3fd90257e3eaebd6199e544e9f200b82b9aac07e
Avadhesh Singh <83873972+singhonscreen@users.noreply.github.com>
2021-10-07 22:19:44 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     <>
       <Router history={history}>
         <Switch>
-          <Route exact path="/githubapi_pagination_data" >
+          <Route exact path="/" >
             <SearchPage setState={setState}  setSearchData = {setSearchData} state={state} searchData = {searchData} />
           </Route>
           <Route path={`/users/${searchData}/repos`} >
